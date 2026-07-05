89eff00d97b8474f327dd48fcd6f74ccd95069d5
Bung Khoai <bungkhoai05@gmail.com>
2026-07-05 18:34:18 +0700

    Giao dien tro choi

--- GameDoanSo/Program.cs
+++ GameDoanSo/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Title = "Game đoán số";
+            Console.WriteLine("=====================");
+            Console.WriteLine();
+            Console.WriteLine("GAME DOAN SO");
+            Console.WriteLine();
+            Console.WriteLine("=====================");
+            Console.WriteLine();
+            Console.WriteLine("Doan so tu 1 den 100");
+            Console.WriteLine("Nhan Enter de bat dau!");
+            Console.ReadLine(); 
         }
     }
 }

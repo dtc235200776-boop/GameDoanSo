2875463ceb105077dc5c04157447c252760c8b43
Bung Khoai <bungkhoai05@gmail.com>
2026-07-05 21:26:03 +0700

    Them mau cho dep hon va tat hien thi secretN

--- GameDoanSo/Program.cs
+++ GameDoanSo/Program.cs
@@ -18,7 +18,7 @@
             {
                 Random random = new Random();
                 int secretNumber = random.Next(1, 101);
-                Console.WriteLine($"So bi mat la: {secretNumber}");
+                //Console.WriteLine($"So bi mat la: {secretNumber}");
                 int guess;
                 bool isCorrect = false;
                 int dem = 0;
@@ -27,26 +27,36 @@
                     Console.Write("Nhap so cua ban: ");
                     if (!int.TryParse(Console.ReadLine(), out guess))
                     {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
                         Console.WriteLine("Vui lòng nhập số!");
+                        Console.ResetColor();
                         continue;
                     }
                     if (guess < 1 || guess > 100)
                     {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
                         Console.WriteLine("So ban nhap khong hop le. Vui long nhap so tu 1 den 100.");
+                        Console.ResetColor();
                         continue;
                     }
                     dem++;
                     if (guess < secretNumber)
                     {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.WriteLine("So ban nho hon so bi mat.");
+                        Console.ResetColor();
                     }
                     else if (guess > secretNumber)
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("So ban lon hon so bi mat.");
+                        Console.ResetColor();
                     }
                     else
                     {
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Chuc mung! Ban da doan dung so bi mat.");
+                        Console.ResetColor();
                         Console.WriteLine($"So lan da doan: {dem}");
                         isCorrect = true;
                     }

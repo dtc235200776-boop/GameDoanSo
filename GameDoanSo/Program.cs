19fc3b82f426fe6c3470000810cab731cf76b859
Bung Khoai <bungkhoai05@gmail.com>
2026-07-05 18:48:00 +0700

    Hoan thanh sinh so ngau nhien

--- GameDoanSo/Program.cs
+++ GameDoanSo/Program.cs
@@ -14,6 +14,9 @@
             Console.WriteLine();
             Console.WriteLine("Doan so tu 1 den 100");
             Console.WriteLine("Nhan Enter de bat dau!");
+            Random random = new Random();
+            int secretNumber = random.Next(1, 101);
+            Console.WriteLine($"So bi mat la: {secretNumber}");
             Console.ReadLine(); 
         }
     }
